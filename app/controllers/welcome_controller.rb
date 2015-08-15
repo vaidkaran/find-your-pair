a7482531f3737632eeb29a922e0f9f0d4a6007ea
Karan Vaid <vaidkaran89@gmail.com>
2015-08-15 09:24:42 +0530

    some more progress, need to remove the current user from similar projects search

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,17 +6,22 @@ class WelcomeController < ApplicationController
   end
 
   def similar_projects
-    user_project_platforms = []
-    current_user.projects.each do |p|
-      user_project_platforms << p.platform.to_s
+    current_user_project_platforms = []
+    current_user.projects.select(:platform).distinct.each do |p|
+      current_user_project_platforms << p.platform
     end
 
-    @record = []
-    user_project_platforms.each do |p|
+    record = []
+    current_user_project_platforms.each do |p|
       sql = "select users.fname, projects.name, projects.platform from users join projects on users.id=projects.user_id where projects.platform='#{p}';"
-      @record << ActiveRecord::Base.connection.execute(sql)
+      record << ActiveRecord::Base.connection.execute(sql)
     end
-    require 'pry'; binding.pry
-    puts "this"
+
+    @similar_platform_projects=[]
+    record.each do |r|
+      r.each(as: :hash) do |result|
+        @similar_platform_projects << result
+      end
+    end
   end
 end
