c674ada57e044159a9886f639c18414000ea7615
Karan Vaid <vaidkaran89@gmail.com>
2016-03-04 18:44:04 +0530

    did some work in controller tests

--- spec/controllers/welcome_spec.rb
+++ spec/controllers/welcome_spec.rb
@@ -1,21 +1,38 @@
 require 'rails_helper'
 
 describe WelcomeController, type: :controller do
-  it 'should return a status of 200 on #index' do
-    get :index
-    expect(response.status).to eq(200)
+
+  describe 'welcome#index' do
+    it 'should return a status of 200 on #index' do
+      get :index
+      expect(response.status).to eq(200)
+    end
+
+    it 'should render the index view for a logged out user' do
+      get :index
+      expect(response).to render_template('welcome/index')
+    end
+
+    it 'should redirect to lihp for a logged in user' do
+      user = FactoryGirl.create(:user)
+      sign_in user
+      get :index
+      expect(response).to redirect_to(lihp_url)
+    end
   end
 
-  it 'should render the index view for a logged out user' do
-    get :index
-    expect(response).to render_template('welcome/index')
-  end
 
-  it 'should render the lihp view for a logged in user' do
-    user = FactoryGirl.create(:user)
-    sign_in user
-    get :index
-    expect(response).to redirect_to(lihp_url)
-  end
+  describe 'welcome#lihp' do
+    before :each do
+      user = FactoryGirl.create(:user)
+      sign_in user
+    end
 
+    it 'should have a valid user in current_user' do
+      expect(controller.current_user.email).to eq 'vaidkaran89@gmail.com'
+    end
+
+    it 'sets @s_t_users with correct values'
+
+  end
 end
