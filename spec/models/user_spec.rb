af46ac10b1f083f2cf9ccb2006b9f57fa64890f4
Karan Vaid <vaidkaran89@gmail.com>
2016-03-11 16:06:49 +0530

    removed the model rspec unit test as it wasn't beneficial

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,2 @@
 require 'rails_helper'
 
-describe User do
-  it('should have the correct first name') do
-    #user = FactoryGirl.build_stubbed(:user)
-    user = FactoryGirl.create(:user)
-    expect(user.fname).to eq('Karan')
-  end
-end
